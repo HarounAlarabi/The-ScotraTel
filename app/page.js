b005be1527129c2ce5acba9f58af747923161b7e
Haroun Alarabi <alarabiharoun@gmail.com>
2024-12-12 15:21:10 +0000

    update app page

--- app/page.js
+++ app/page.js
@@ -10,16 +10,17 @@ export default function Page() {
         fill
         alt="Mountains and forests with two cabins"
         placeholder="blur"
-        className="object-cover object-top"
+        className=" object-fill"
       />
 
       <div className="relative z-10 text-center">
         <h1 className="text-8xl text-primary-50 mb-20 tracking-tight font-normal">
           Welcome to paradise.
         </h1>
+
         <Link
           href="/cabins"
-          className="bg-accent-500  px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+          className="px-8 py-4 text-primary-50 text-lg font-semibold hover:bg-accent-600 rounded-full border bg-accent-600 transition-all mt-11"
         >
           Explore luxury cabins
         </Link>
